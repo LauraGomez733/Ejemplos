4d8a995d85a70f36b6e38d8306e8f9b17659251c
lvgomez <lvgomez820@ucatolica.edu.co>
2026-03-03 19:37:27 -0500

    Mejora validacion de fecha

--- incrementarFecha.py
+++ incrementarFecha.py
@@ -3,13 +3,16 @@ mes = int(input("Ingrese el mes: "))
 anio = int(input("Ingrese el anio: "))
 
 dias_mes = [31,28,31,30,31,30,31,31,30,31,30,31]
+if mes < 1 or mes > 12:
+    print ("Mes invalido")
+    exit()
 
 if (anio % 4 == 0 and anio % 100 !=0) or (anio % 400 = 0):
     dias_mes[1] = 29
 
 dia += 1
 
-if data > dias_mes[mes -1]:
+if dia > dias_mes[mes -1]:
     dia = 1
     mes += 1
     if mes > 12:
